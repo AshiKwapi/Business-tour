c14ef4853166b38ac1a303ac5700381eee33df92
AshiKwapi <155096167+AshiKwapi@users.noreply.github.com>
2023-12-28 18:33:19 +0100

    Create Pola.java

---
+++ Pola.java
@@ -0,0 +1,52 @@
+package businesstour;
+
+
+public class Pola {
+
+    String nazwa_pola;
+    int koszt_kupna_pola;
+    int koszt_1_domu;
+    int koszt_2_domow;
+    int koszt_3_domow;
+    int koszt_hotelu;
+    double czynsz_pole;
+    double czynsz_1_dom;
+    double czynsz_2_dom ;
+    double czynsz_3_dom;
+    double czynsz_hotel;
+    public Pola (){}
+    public String nazwa_pola(){
+    return this.nazwa_pola;
+}
+    public int koszt_kupna_pola(){
+    return this.koszt_kupna_pola;
+}
+     public int koszt_1_domu(){
+    return this.koszt_1_domu;
+}
+     public int koszt_2_domow(){
+    return this.koszt_2_domow;
+}
+     public int koszt_3_domow(){
+    return this.koszt_3_domow;
+}
+     public int koszt_hotelu(){
+    return this.koszt_hotelu;
+}
+     public double czynsz_pole(){
+    return this.czynsz_pole;
+}
+     public double czynsz_1_dom(){
+    return this.czynsz_1_dom;
+}
+     public double czynsz_2_dom (){
+    return this.czynsz_2_dom;
+}
+     public double czynsz_3_dom(){
+    return this.czynsz_3_dom;
+}
+     public double czynsz_hotel(){
+    return this.czynsz_hotel;
+}
+}
+
